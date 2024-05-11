ac6f1091265ff90eaf6742c9c0930de6b1046f9b
yunweneric <yunweneric@gmail.com>
2024-05-11 20:53:51 +0100

    Final implementation uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter 3D Carousel Slider',
+      title: 'Flutter 3D Carousel',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
